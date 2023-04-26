b278e507dedd08ddb6d971243e7a9238b8bd6506
Jerome Gravel-Niquet <jeromegn@gmail.com>
2023-04-26 14:02:03 -0400

    fix buildkite

--- .buildkite/upload.sh
+++ .buildkite/upload.sh
@@ -10,10 +10,10 @@ buildkite-agent artifact download 'builds/*.tar.gz' ./builds
 BUILD_NAME="${BUILDKITE_BRANCH//[^a-zA-Z0-9-]/--}"
 
 # upload
-aws s3 cp builds/corrosion.tar.gz s3://flyio-builds/corrosion/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz --region us-east-2 &
-aws s3 cp builds/corro-dns.tar.gz s3://flyio-builds/corro-consul/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz --region us-east-2 &
+aws s3 cp builds/corrosion.tar.gz s3://flyio-builds/corrosion2/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz --region us-east-2 &
+aws s3 cp builds/corro-consul.tar.gz s3://flyio-builds/corro-consul/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz --region us-east-2 &
 wait
 
-aws s3 cp s3://flyio-builds/corrosion/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz s3://flyio-builds/corrosion/${BUILD_NAME}.tar.gz --region us-east-2 &
-aws s3 cp s3://flyio-builds/corro-consul/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz s3://flyio-builds/corro-dns/${BUILD_NAME}.tar.gz --region us-east-2 &
+aws s3 cp s3://flyio-builds/corrosion2/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz s3://flyio-builds/corrosion2/${BUILD_NAME}.tar.gz --region us-east-2 &
+aws s3 cp s3://flyio-builds/corro-consul/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz s3://flyio-builds/corro-consul/${BUILD_NAME}.tar.gz --region us-east-2 &
 wait
