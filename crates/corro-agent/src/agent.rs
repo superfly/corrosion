148122fdfa2ec8adb2d952b1015b60d77e0c1a1e
Jerome Gravel-Niquet <jeromegn@gmail.com>
2023-08-24 10:47:49 -0400

    set back the bookkeeping compaction to 5 minutes

--- crates/corro-agent/src/agent.rs
+++ crates/corro-agent/src/agent.rs
@@ -73,7 +73,7 @@ use trust_dns_resolver::{
 
 const MAX_SYNC_BACKOFF: Duration = Duration::from_secs(60); // 1 minute oughta be enough, we're constantly getting broadcasts randomly + targetted
 const RANDOM_NODES_CHOICES: usize = 10;
-const COMPACT_BOOKED_INTERVAL: Duration = Duration::from_secs(60);
+const COMPACT_BOOKED_INTERVAL: Duration = Duration::from_secs(300);
 
 pub struct AgentOptions {
     actor_id: ActorId,
