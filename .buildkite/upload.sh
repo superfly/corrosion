7f415ac0b785b0b520e10db28d5918570bca30b2
Jerome Gravel-Niquet <jeromegn@gmail.com>
2023-09-18 07:59:58 -0400

    remove a few corrosion2 mentions, tweak readme

--- .buildkite/upload.sh
+++ .buildkite/upload.sh
@@ -10,8 +10,8 @@ buildkite-agent artifact download 'builds/*.tar.gz' ./builds
 BUILD_NAME="${BUILDKITE_BRANCH//[^a-zA-Z0-9-]/--}"
 
 # upload
-aws s3 cp builds/corrosion.tar.gz s3://flyio-builds/corrosion2/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz --region us-east-2 &
+aws s3 cp builds/corrosion.tar.gz ${UPLOAD_BUCKET_PATH}/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz --region us-east-2 &
 wait
 
-aws s3 cp s3://flyio-builds/corrosion2/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz s3://flyio-builds/corrosion2/${BUILD_NAME}.tar.gz --region us-east-2 &
+aws s3 cp ${UPLOAD_BUCKET_PATH}/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz ${UPLOAD_BUCKET_PATH}/${BUILD_NAME}.tar.gz --region us-east-2 &
 wait
