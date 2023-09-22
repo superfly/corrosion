004331ba156d06a4340a99c5ed04c4be14abef2f
catflydotio <95245363+catflydotio@users.noreply.github.com>
2023-09-22 12:03:23 -0400

    fly.io example updates (#63)

--- examples/fly/entrypoint.sh
+++ examples/fly/entrypoint.sh
@@ -3,6 +3,8 @@
 # Add gossip and bootstrap addresses to Corrosion config file before 
 # starting agent with Dockerfile CMD
 sed -i 's/\[gossip\]/&\naddr = "['${FLY_PRIVATE_IP}']:8787"\
-bootstrap = ["'${FLY_APP_NAME}'.internal:8787"]/' /app/config.toml
+bootstrap = ["'${FLY_APP_NAME}'.internal:8787"]/' /etc/corrosion/config.toml
+
+su - corrosion
 
 exec "$@"
