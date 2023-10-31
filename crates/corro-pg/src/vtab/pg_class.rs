e65d65cf211d0dca2255215f7bdd5505d8cd64a2
Jerome Gravel-Niquet <jeromegn@gmail.com>
2023-10-31 08:50:38 -0400

    forgot a few files

---
+++ crates/corro-pg/src/vtab/pg_class.rs
@@ -0,0 +1,114 @@
+use std::{marker::PhantomData, os::raw::c_int};
+
+use rusqlite::vtab::{
+    sqlite3_vtab, sqlite3_vtab_cursor, IndexInfo, VTab, VTabConnection, VTabCursor, Values,
+};
+
+#[repr(C)]
+pub struct PgClassTable {
+    /// Base class. Must be first
+    base: sqlite3_vtab,
+}
+
+unsafe impl<'vtab> VTab<'vtab> for PgClassTable {
+    type Aux = ();
+    type Cursor = PgClassTableCursor<'vtab>;
+
+    fn connect(
+        _: &mut VTabConnection,
+        _aux: Option<&()>,
+        _args: &[&[u8]],
+    ) -> rusqlite::Result<(String, PgClassTable)> {
+        let vtab = PgClassTable {
+            base: sqlite3_vtab::default(),
+        };
+
+        Ok((
+            "CREATE TABLE pg_class (
+                oid                 INTEGER,
+                relname             TEXT,
+                relnamespace        INTEGER,
+                reltype             INTEGER,
+                reloftype           INTEGER,
+                relowner            INTEGER,
+                relam               INTEGER,
+                relfilenode         INTEGER,
+                reltablespace       INTEGER,
+                relpages            INTEGER,
+                reltuples           REAL,
+                relallvisible       INTEGER,
+                reltoastrelid       INTEGER,
+                relhasindex         INTEGER,
+                relisshared         INTEGER,
+                relpersistence      TEXT,
+                relkind             TEXT,
+                relnatts            INTEGER,
+                relchecks           INTEGER,
+                relhasrules         INTEGER,
+                relhastriggers      INTEGER,
+                relhassubclass      INTEGER,
+                relrowsecurity      INTEGER,
+                relforcerowsecurity INTEGER,
+                relispopulated      INTEGER,
+                relreplident        TEXT,
+                relispartition      INTEGER,
+                relrewrite          INTEGER,
+                relfrozenxid        INTEGER,
+                relminmxid          INTEGER,
+                relacl              TEXT,
+                reloptions          TEXT,
+                relpartbound        TEXT
+		    )"
+            .into(),
+            vtab,
+        ))
+    }
+
+    fn best_index(&self, info: &mut IndexInfo) -> rusqlite::Result<()> {
+        info.set_estimated_cost(1.);
+        Ok(())
+    }
+
+    fn open(&'vtab mut self) -> rusqlite::Result<PgClassTableCursor<'vtab>> {
+        Ok(PgClassTableCursor::default())
+    }
+}
+
+#[derive(Default)]
+#[repr(C)]
+pub struct PgClassTableCursor<'vtab> {
+    /// Base class. Must be first
+    base: sqlite3_vtab_cursor,
+    /// The rowid
+    row_id: i64,
+    phantom: PhantomData<&'vtab PgClassTable>,
+}
+
+unsafe impl VTabCursor for PgClassTableCursor<'_> {
+    fn filter(
+        &mut self,
+        _idx_num: c_int,
+        _idx_str: Option<&str>,
+        _args: &Values<'_>,
+    ) -> rusqlite::Result<()> {
+        self.row_id = 1;
+        Ok(())
+    }
+
+    fn next(&mut self) -> rusqlite::Result<()> {
+        self.row_id += 1;
+        Ok(())
+    }
+
+    fn eof(&self) -> bool {
+        true // no rows...
+    }
+
+    fn column(&self, _ctx: &mut rusqlite::vtab::Context, _col: c_int) -> rusqlite::Result<()> {
+        Ok(())
+    }
+
+    fn rowid(&self) -> rusqlite::Result<i64> {
+        Ok(self.row_id)
+    }
+}
---
+++ crates/corro-pg/src/vtab/pg_namespace.rs
@@ -0,0 +1,105 @@
+use std::{marker::PhantomData, os::raw::c_int};
+
+use rusqlite::vtab::{
+    sqlite3_vtab, sqlite3_vtab_cursor, IndexInfo, VTab, VTabConnection, VTabCursor, Values,
+};
+
+const PG_NAMESPACES: &[(i64, &str, i64, &str)] = &[
+    (99, "pg_toast", 10, ""),
+    (11, "pg_catalog", 10, ""),
+    (2200, "public", 10, ""),
+    (13427, "information_schema", 10, ""),
+];
+
+#[repr(C)]
+pub struct PgNamespaceTable {
+    /// Base class. Must be first
+    base: sqlite3_vtab,
+}
+
+unsafe impl<'vtab> VTab<'vtab> for PgNamespaceTable {
+    type Aux = ();
+    type Cursor = PgNamespaceTableCursor<'vtab>;
+
+    fn connect(
+        _: &mut VTabConnection,
+        _aux: Option<&()>,
+        _args: &[&[u8]],
+    ) -> rusqlite::Result<(String, PgNamespaceTable)> {
+        let vtab = PgNamespaceTable {
+            base: sqlite3_vtab::default(),
+        };
+
+        Ok((
+            "CREATE TABLE pg_namespace (
+                oid 	INTEGER,
+                nspname  TEXT,
+                nspowner INTEGER,
+                nspacl   TEXT
+		    )"
+            .into(),
+            vtab,
+        ))
+    }
+
+    fn best_index(&self, info: &mut IndexInfo) -> rusqlite::Result<()> {
+        info.set_estimated_cost(1.);
+        Ok(())
+    }
+
+    fn open(&'vtab mut self) -> rusqlite::Result<PgNamespaceTableCursor<'vtab>> {
+        Ok(PgNamespaceTableCursor::default())
+    }
+}
+
+#[derive(Default)]
+#[repr(C)]
+pub struct PgNamespaceTableCursor<'vtab> {
+    /// Base class. Must be first
+    base: sqlite3_vtab_cursor,
+    /// The rowid
+    row_id: i64,
+    phantom: PhantomData<&'vtab PgNamespaceTable>,
+}
+
+unsafe impl VTabCursor for PgNamespaceTableCursor<'_> {
+    fn filter(
+        &mut self,
+        _idx_num: c_int,
+        _idx_str: Option<&str>,
+        _args: &Values<'_>,
+    ) -> rusqlite::Result<()> {
+        self.row_id = 1;
+        Ok(())
+    }
+
+    fn next(&mut self) -> rusqlite::Result<()> {
+        self.row_id += 1;
+        Ok(())
+    }
+
+    fn eof(&self) -> bool {
+        self.row_id >= PG_NAMESPACES.len() as i64
+    }
+
+    fn column(&self, ctx: &mut rusqlite::vtab::Context, col: c_int) -> rusqlite::Result<()> {
+        if let Some(pg_ns) = PG_NAMESPACES.get(self.row_id as usize) {
+            match col {
+                0 => ctx.set_result(&pg_ns.0),
+                1 => ctx.set_result(&pg_ns.1),
+                2 => ctx.set_result(&pg_ns.2),
+                3 => ctx.set_result(&pg_ns.3),
+                _ => Err(rusqlite::Error::InvalidColumnIndex(col as usize)),
+            }
+        } else {
+            Err(rusqlite::Error::ModuleError(format!(
+                "pg namespace out of bound (row id: {})",
+                self.row_id
+            )))
+        }
+    }
+
+    fn rowid(&self) -> rusqlite::Result<i64> {
+        Ok(self.row_id)
+    }
+}
