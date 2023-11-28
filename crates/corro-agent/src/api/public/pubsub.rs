625fdb2a7b3b1ab5051d0fa5c6cc68aef17d723b
Jerome Gravel-Niquet <jeromegn@gmail.com>
2023-11-28 10:39:44 -0500

    unsub / cleanup faster from slow-to-resub clients

--- crates/corro-agent/src/api/public/pubsub.rs
+++ crates/corro-agent/src/api/public/pubsub.rs
@@ -108,7 +108,7 @@ fn make_query_event_bytes(
     Ok((buf.split().freeze(), query_evt.meta()))
 }
 
-const MAX_UNSUB_TIME: Duration = Duration::from_secs(300);
+const MAX_UNSUB_TIME: Duration = Duration::from_secs(120);
 // this should be a fraction of the MAX_UNSUB_TIME
 const RECEIVERS_CHECK_INTERVAL: Duration = Duration::from_secs(30);
 
