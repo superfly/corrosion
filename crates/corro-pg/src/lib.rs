404feed14b957f34182294dac7f12fe92092a751
Jerome Gravel-Niquet <jeromegn@gmail.com>
2023-11-30 14:56:25 -0500

    forgot to save corro-pg file

--- crates/corro-pg/src/lib.rs
+++ crates/corro-pg/src/lib.rs
@@ -2351,12 +2351,14 @@ fn handle_commit(agent: &Agent, conn: &Connection) -> rusqlite::Result<()> {
 
             block_in_place(|| {
                 // TODO: make this more generic so both sync and local changes can use it.
-                let mut prepped = conn.prepare_cached(r#"
-                SELECT "table", pk, cid, val, col_version, db_version, seq, COALESCE(site_id, crsql_site_id()), cl
+                let mut prepped = conn.prepare_cached(
+                    r#"
+                SELECT "table", pk, cid, val, col_version, db_version, seq, site_id, cl
                     FROM crsql_changes
                     WHERE db_version = ?
                     ORDER BY seq ASC
-            "#)?;
+            "#,
+                )?;
                 let rows = prepped.query_map([db_version], row_to_change)?;
                 let chunked = ChunkedChanges::new(rows, 0, last_seq, MAX_CHANGES_BYTE_SIZE);
                 for changes_seqs in chunked {
