5cd7264b5e817f5bdf81c690efd7f8c2707635f7
Katharina Fey <kookie@spacekookie.de>
2023-12-13 18:10:18 +0100

    corro-agent: move startup utilties and tasks to separate modules
    - run_root: contains the starting point for the agent
    - handlers: any handler task that doesn't directly fit into bi or uni
                payloads.  More will be added in future, but any that
                don't have another home can live here.

---
+++ crates/corro-agent/src/agent/handlers.rs
@@ -0,0 +1,188 @@
+//!
+
+use crate::agent::{bcast::spawn_unipayload_handler, sync::spawn_bipayload_handler};
+use corro_types::{
+    agent::Agent,
+    broadcast::{FocaInput, UniPayload},
+};
+use metrics::increment_counter;
+use spawn::spawn_counted;
+use std::{net::SocketAddr, time::Duration};
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio_stream::{wrappers::ReceiverStream, StreamExt};
+use tracing::{debug, error};
+use tripwire::{Outcome, PreemptibleFutureExt, Tripwire};
+
+/// Spawn a tree of tasks that handles incoming gossip server
+/// connections, streams, and their respective payloads.
+pub fn spawn_gossipserver_handler(
+    agent: &Agent,
+    tripwire: &Tripwire,
+    gossip_server_endpoint: quinn::Endpoint,
+    process_uni_tx: Sender<UniPayload>,
+) {
+    spawn_counted({
+        let agent = agent.clone();
+        let mut tripwire = tripwire.clone();
+        async move {
+            loop {
+                let connecting = match gossip_server_endpoint
+                    .accept()
+                    .preemptible(&mut tripwire)
+                    .await
+                {
+                    Outcome::Completed(Some(connecting)) => connecting,
+                    Outcome::Completed(None) => return,
+                    Outcome::Preempted(_) => break,
+                };
+
+                // Spawn incoming connection handlers
+                spawn_incoming_connection_handlers(&agent, &tripwire, process_uni_tx, connecting);
+            }
+
+            // graceful shutdown
+            gossip_server_endpoint.reject_new_connections();
+            _ = gossip_server_endpoint
+                .wait_idle()
+                .with_timeout(Duration::from_secs(5))
+                .await;
+            gossip_server_endpoint.close(0u32.into(), b"shutting down");
+        }
+    });
+}
+
+/// Spawn a task which handles all state and interactions for a given
+/// incoming connection.  This function spawns many futures!
+pub fn spawn_incoming_connection_handlers(
+    agent: &Agent,
+    tripwire: &Tripwire,
+    process_uni_tx: Sender<UniPayload>,
+    connecting: quinn::Connecting,
+) {
+    let process_uni_tx = process_uni_tx.clone();
+    let agent = agent.clone();
+    let tripwire = tripwire.clone();
+    tokio::spawn(async move {
+        let remote_addr = connecting.remote_address();
+        // let local_ip = connecting.local_ip().unwrap();
+        debug!("got a connection from {remote_addr}");
+
+        let conn = match connecting.await {
+            Ok(conn) => conn,
+            Err(e) => {
+                error!("could not handshake connection from {remote_addr}: {e}");
+                return;
+            }
+        };
+
+        increment_counter!("corro.peer.connection.accept.total");
+
+        debug!("accepted a QUIC conn from {remote_addr}");
+
+        // Spawn handler tasks for this connection
+        spawn_foca_handler(&agent, &tripwire, &conn);
+        spawn_unipayload_handler(&agent, &tripwire, &conn, process_uni_tx);
+        spawn_bipayload_handler(&agent, &tripwire, &conn);
+    });
+}
+
+/// Spawn a single task that accepts chunks from a receiver and
+/// updates cluster member round-trip-times in the agent state.
+pub fn spawn_rtt_handler(agent: &Agent, rtt_rx: Receiver<(SocketAddr, Duration)>) {
+    tokio::spawn({
+        let agent = agent.clone();
+        async move {
+            let stream = ReceiverStream::new(rtt_rx);
+            // we can handle a lot of them I think...
+            let chunker = stream.chunks_timeout(1024, Duration::from_secs(1));
+            tokio::pin!(chunker);
+            while let Some(chunks) = StreamExt::next(&mut chunker).await {
+                let mut members = agent.members().write();
+                for (addr, rtt) in chunks {
+                    members.add_rtt(addr, rtt);
+                }
+            }
+        }
+    });
+}
+
+/// Spawn a single task to listen for `Datagram`s from the transport
+/// and apply FOCA messages to the local SWIM statemachine.
+pub fn spawn_foca_handler(agent: &Agent, tripwire: &Tripwire, conn: &quinn::Connection) {
+    tokio::spawn({
+        let conn = conn.clone();
+        let mut tripwire = tripwire.clone();
+        let foca_tx = agent.tx_foca().clone();
+        async move {
+            loop {
+                let b = tokio::select! {
+                    b_res = conn.read_datagram() => match b_res {
+                        Ok(b) => {
+                            increment_counter!("corro.peer.datagram.recv.total");
+                            counter!("corro.peer.datagram.bytes.recv.total", b.len() as u64);
+                            b
+                        },
+                        Err(e) => {
+                            debug!("could not read datagram from connection: {e}");
+                            return;
+                        }
+                    },
+                    _ = &mut tripwire => {
+                        debug!("connection cancelled");
+                        return;
+                    }
+                };
+
+                if let Err(e) = foca_tx.send(FocaInput::Data(b)).await {
+                    error!("could not send data foca input: {e}");
+                }
+            }
+        }
+    });
+}
+
+pub fn spawn_backoff_handler(agent: &Agent, gossip_addr: SocketAddr) {
+    tokio::spawn({
+        let agent = agent.clone();
+        async move {
+            let mut boff = backoff::Backoff::new(10)
+                .timeout_range(Duration::from_secs(5), Duration::from_secs(120))
+                .iter();
+            let timer = tokio::time::sleep(Duration::new(0, 0));
+            tokio::pin!(timer);
+
+            loop {
+                timer.as_mut().await;
+
+                match generate_bootstrap(
+                    agent.config().gossip.bootstrap.as_slice(),
+                    gossip_addr,
+                    agent.pool(),
+                )
+                .await
+                {
+                    Ok(addrs) => {
+                        for addr in addrs.iter() {
+                            debug!("Bootstrapping w/ {addr}");
+                            if let Err(e) = agent
+                                .tx_foca()
+                                .send(FocaInput::Announce((*addr).into()))
+                                .await
+                            {
+                                error!("could not send foca Announce message: {e}");
+                            } else {
+                                debug!("successfully sent announce message");
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        error!("could not find nodes to announce ourselves to: {e}");
+                    }
+                }
+
+                let dur = boff.next().unwrap_or(ANNOUNCE_INTERVAL);
+                timer.as_mut().reset(tokio::time::Instant::now() + dur);
+            }
+        }
+    });
+}
---
+++ crates/corro-agent/src/agent/run_root.rs
@@ -0,0 +1,171 @@
+//! Start the root agent tasks
+//!
+//! 1. foo
+//! 2. bar
+//! 3. spawn `super::other_module`
+
+use quinn::Connecting;
+use std::sync::Arc;
+use tokio::sync::{mpsc::channel, RwLock as TokioRwLock};
+use tracing::{debug, error, info};
+use tripwire::Tripwire;
+
+use crate::{
+    agent::{bcast, setup, sync, AgentOptions},
+    api::public::pubsub::{
+        process_sub_channel, MatcherBroadcastCache, SharedMatcherBroadcastCache,
+    },
+    broadcast::runtime_loop,
+};
+use corro_types::{
+    actor::Actor,
+    agent::Agent,
+    broadcast::BroadcastV1,
+    config::Config,
+    pubsub::{Matcher, SubsManager},
+};
+
+/// Start a new agent with an existing configuration
+///
+/// First initialise `AgentOptions` state via `setup()`, then spawn a
+/// new task that runs the main agent state machine
+pub async fn start_with_config(conf: Config, tripwire: Tripwire) -> eyre::Result<Agent> {
+    let (agent, opts) = setup(conf, tripwire.clone()).await?;
+
+    tokio::spawn({
+        let agent = agent.clone();
+        async move {
+            match run(agent, opts).await {
+                Ok(_) => info!("corrosion agent run is done"),
+                Err(e) => error!("running corrosion agent failed: {e}"),
+            }
+        }
+    });
+
+    Ok(agent)
+}
+
+async fn run(agent: Agent, opts: AgentOptions) -> eyre::Result<()> {
+    let AgentOptions {
+        actor_id,
+        gossip_server_endpoint,
+        transport,
+        api_listener,
+        mut tripwire,
+        rx_bcast,
+        rx_apply,
+        rx_empty,
+        rx_clear_buf,
+        rx_changes,
+        rx_foca,
+        subs_manager,
+        rtt_rx,
+    } = opts;
+
+    let subs_bcast_cache = setup_spawn_subscriptions(&agent, &subs_manager, &tripwire).await;
+
+    //// Start PG server to accept query requests from PG clients
+    // TODO: pull this out into a separate function
+    if let Some(pg_conf) = agent.config().api.pg.clone() {
+        info!("Starting PostgreSQL wire-compatible server");
+        let pg_server = corro_pg::start(agent.clone(), pg_conf, tripwire.clone()).await?;
+        info!(
+            "Started PostgreSQL wire-compatible server, listening at {}",
+            pg_server.local_addr
+        );
+    }
+
+    //// TODO: turn channels into a named and counted resource
+    let (to_send_tx, to_send_rx) = channel(10240);
+    let (notifications_tx, notifications_rx) = channel(10240);
+    let (bcast_msg_tx, bcast_rx) = channel::<BroadcastV1>(10240);
+    let (process_uni_tx, mut process_uni_rx) = channel(10240);
+
+    //// DOCME: why here and why now?
+    let gossip_addr = gossip_server_endpoint.local_addr()?;
+
+    //// Start the main SWIM runtime loop
+    runtime_loop(
+        Actor::new(
+            actor_id,
+            agent.external_addr().unwrap_or_else(|| agent.gossip_addr()),
+            agent.clock().new_timestamp().into(),
+        ),
+        agent.clone(),
+        transport.clone(),
+        rx_foca,
+        rx_bcast,
+        to_send_tx,
+        notifications_tx,
+        tripwire.clone(),
+    );
+
+    //// Start async uni-broadcast message decoder
+    bcast::spawn_message_decoder(&agent, process_uni_rx, bcast_msg_tx);
+
+    // TODO: start big_boi::spawn_lots_of_things
+
+    info!("Starting peer API on udp/{gossip_addr} (QUIC)");
+
+    Ok(())
+}
+
+/// Initialise subscription state and tasks
+///
+/// 1. Get subscriptions state directory from config
+/// 2. Load existing subscriptions and restore them in SubsManager
+/// 3. Spawn subscription processor task
+async fn setup_spawn_subscriptions(
+    agent: &Agent,
+    subs_manager: &SubsManager,
+    tripwire: &Tripwire,
+) -> eyre::Result<SharedMatcherBroadcastCache> {
+    let mut subs_bcast_cache = MatcherBroadcastCache::default();
+    let mut to_cleanup = vec![];
+
+    let subs_path = agent.config().db.subscriptions_path();
+
+    if let Ok(mut dir) = tokio::fs::read_dir(&subs_path).await {
+        while let Ok(Some(entry)) = dir.next_entry().await {
+            let path_str = entry.path().display().to_string();
+            if let Some(sub_id_str) = path_str.strip_prefix(subs_path.as_str()) {
+                if let Ok(sub_id) = sub_id_str.trim_matches('/').parse() {
+                    let (_, created) = match subs_manager.restore(
+                        sub_id,
+                        &subs_path,
+                        &agent.schema().read(),
+                        agent.pool(),
+                        tripwire.clone(),
+                    ) {
+                        Ok(res) => res,
+                        Err(e) => {
+                            error!(%sub_id, "could not restore subscription: {e}");
+                            to_cleanup.push(sub_id);
+                            continue;
+                        }
+                    };
+
+                    info!(%sub_id, "Restored subscription");
+
+                    let (sub_tx, _) = tokio::sync::broadcast::channel(10240);
+
+                    tokio::spawn(process_sub_channel(
+                        subs_manager.clone(),
+                        sub_id,
+                        sub_tx.clone(),
+                        created.evt_rx,
+                    ));
+
+                    subs_bcast_cache.insert(sub_id, sub_tx);
+                }
+            }
+        }
+    }
+
+    for id in to_cleanup {
+        info!(sub_id = %id, "Cleaning up unclean subscription");
+        Matcher::cleanup(id, Matcher::sub_path(subs_path.as_path(), id))?;
+    }
+
+    Ok(Arc::new(TokioRwLock::new(subs_bcast_cache)))
+}
