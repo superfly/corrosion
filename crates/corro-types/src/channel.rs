d5ccc4dedbf887bc3cf4161389af0b28ed050370
Katharina Fey <kookie@spacekookie.de>
2024-01-29 15:36:57 +0100

    Tweak and add CorroChannel metrics

--- crates/corro-types/src/channel.rs
+++ crates/corro-types/src/channel.rs
@@ -1,24 +1,31 @@
-use metrics::{gauge, Gauge};
+use metrics::{counter, gauge, histogram, Counter, Histogram};
 use std::{
     fmt::{self, Debug, Formatter},
-    time::Duration,
+    time::{Duration, Instant},
 };
-use tokio::sync::mpsc::{
-    channel,
-    error::{SendError, SendTimeoutError, TryRecvError, TrySendError},
-    Receiver, Sender,
+use tokio::{
+    sync::mpsc::{
+        channel,
+        error::{SendError, SendTimeoutError, TryRecvError, TrySendError},
+        Receiver, Sender,
+    },
+    time::sleep,
 };
 
-/// A tokio channel wrapper sender that tracks occupancy
+/// A tokio channel wrapper sender that tracks various metrics
 pub struct CorroSender<T> {
-    gauge: Gauge,
+    send_count: Counter,
+    failed_sends: Counter,
+    send_time: Histogram,
     inner: Sender<T>,
 }
 
 impl<T> Clone for CorroSender<T> {
     fn clone(&self) -> Self {
         Self {
-            gauge: self.gauge.clone(),
+            send_count: self.send_count.clone(),
+            failed_sends: self.failed_sends.clone(),
+            send_time: self.send_time.clone(),
             inner: self.inner.clone(),
         }
     }
@@ -30,9 +37,9 @@ impl<T: Debug> Debug for CorroSender<T> {
     }
 }
 
-/// A tokio channel wrapper receiver that tracks occupancy
+/// A tokio channel wrapper receiver that tracks various metrics
 pub struct CorroReceiver<T> {
-    gauge: Gauge,
+    recv_count: Counter,
     inner: Receiver<T>,
 }
 
@@ -42,33 +49,79 @@ impl<T: Debug> Debug for CorroReceiver<T> {
     }
 }
 
-/// Create a bounded channel which tracks occupancy with a label
-pub fn bounded<T>(max: usize, label: &'static str) -> (CorroSender<T>, CorroReceiver<T>) {
-    let gauge = gauge!("corro.runtime.channel_occupancy", "max_capacity" => max.to_string(), "label" => label);
-    let (tx, rx) = channel(max);
+/// Create a bounded channel which tracks capacity with a label
+pub fn bounded<T: Send + 'static>(
+    capacity: usize,
+    label: &'static str,
+) -> (CorroSender<T>, CorroReceiver<T>) {
+    gauge!("corro.runtime.channel.capacity", "channel_name" => label).set(capacity as f64);
+
+    // Count the number of sends and receives going through the channel
+    let send_count = counter!("corro.runtime.channel.send_count", "channel_name" => label);
+    let recv_count = counter!("corro.runtime.channel.recv_count", "channel_name" => label);
+
+    // How many times did we fail to send
+    let failed_sends = counter!("corro.runtime.channel.failed_send_count", "channel_name" => label);
+
+    // Track current capacity and send time over time
+    let capacity_gauge = gauge!("corro.runtime.channel.capacity", "channel_name" => label);
+    let send_time = histogram!("corro.runtime.channel.send_delay", "channel_name" => label);
+
+    let (tx, rx) = channel(capacity);
+
+    let inner_channel = tx.clone();
+    tokio::spawn(async move {
+        loop {
+            sleep(Duration::from_secs(1)).await;
+            if inner_channel.is_closed() {
+                break;
+            }
+            capacity_gauge.set(inner_channel.capacity() as f64);
+        }
+    });
 
     (
         CorroSender {
-            gauge: gauge.clone(),
+            send_count,
+            failed_sends,
+            send_time,
             inner: tx,
         },
-        CorroReceiver { gauge, inner: rx },
+        CorroReceiver {
+            recv_count,
+            inner: rx,
+        },
     )
 }
 
 impl<T> CorroSender<T> {
     pub async fn send(&self, value: T) -> Result<(), SendError<T>> {
-        self.inner.send(value).await.map(|r| {
-            self.gauge.increment(1.0);
-            r
-        })
+        let before = Instant::now();
+        self.inner
+            .send(value)
+            .await
+            .map(|r| {
+                self.send_time.record(before.elapsed().as_secs_f64());
+                self.send_count.increment(1);
+                r
+            })
+            .map_err(|e| {
+                self.failed_sends.increment(1);
+                e
+            })
     }
 
     pub fn try_send(&self, value: T) -> Result<(), TrySendError<T>> {
-        self.inner.try_send(value).map(|r| {
-            self.gauge.increment(1.0);
-            r
-        })
+        self.inner
+            .try_send(value)
+            .map(|r| {
+                self.send_count.increment(1);
+                r
+            })
+            .map_err(|e| {
+                self.failed_sends.increment(1);
+                e
+            })
     }
 
     pub async fn send_timeout(
@@ -76,31 +129,40 @@ impl<T> CorroSender<T> {
         value: T,
         timeout: Duration,
     ) -> Result<(), SendTimeoutError<T>> {
-        self.inner.send_timeout(value, timeout).await.map(|r| {
-            self.gauge.increment(1.0);
-            r
-        })
+        let before = Instant::now();
+        self.inner
+            .send_timeout(value, timeout)
+            .await
+            .map(|r| {
+                self.send_time.record(before.elapsed().as_secs_f64());
+                self.send_count.increment(1);
+                r
+            })
+            .map_err(|e| {
+                self.failed_sends.increment(1);
+                e
+            })
     }
 }
 
 impl<T> CorroReceiver<T> {
     pub async fn recv(&mut self) -> Option<T> {
         self.inner.recv().await.map(|r| {
-            self.gauge.decrement(1.0);
+            self.recv_count.increment(1);
             r
         })
     }
 
     pub fn try_recv(&mut self) -> Result<T, TryRecvError> {
         self.inner.try_recv().map(|r| {
-            self.gauge.decrement(1.0);
+            self.recv_count.increment(1);
             r
         })
     }
 
     pub fn blocking_recv(&mut self) -> Option<T> {
         self.inner.blocking_recv().map(|r| {
-            self.gauge.decrement(1.0);
+            self.recv_count.increment(1);
             r
         })
     }
