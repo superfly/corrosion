8988ff1367acc28dae7dbed2141f96f9a60344f0
Katharina Fey <kookie@spacekookie.de>
2024-03-12 16:27:32 +0100

    Fix warnings

--- crates/corro-agent/examples/chill_test.rs
+++ crates/corro-agent/examples/chill_test.rs
@@ -1,28 +1,23 @@
-use corro_tests::{launch_test_agent, TestAgent};
+use corro_tests::launch_test_agent;
 use corro_types::api::Statement;
 use corro_types::{
     actor::ActorId,
     api::{ExecResponse, ExecResult},
     sync::generate_sync,
 };
-use futures::{StreamExt, TryStreamExt};
 use hyper::StatusCode;
-use rand::{prelude::*, rngs::StdRng, SeedableRng};
 use serde_json::json;
-use spawn::wait_for_all_pending_handles;
 use std::{
     collections::HashMap,
-    net::SocketAddr,
     time::{Duration, Instant},
 };
 use tokio::time::MissedTickBehavior;
-use tracing::{debug, info_span};
 use tripwire::Tripwire;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 1)]
 async fn main() -> eyre::Result<()> {
     _ = tracing_subscriber::fmt::try_init();
-    let (tripwire, tripwire_worker, tripwire_tx) = Tripwire::new_simple();
+    let (tripwire, _tripwire_worker, _tripwire_tx) = Tripwire::new_simple();
 
     let gossip_a = "127.0.0.1:9999".parse().unwrap();
     let gossip_b = "127.0.0.1:9090".parse().unwrap();
