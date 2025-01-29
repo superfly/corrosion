dc50d81cd57079e57fc30b2a8881fecb479c20bc
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2025-01-29 20:44:53 +0100

    okay, that didn't work out

--- crates/corro-agent/src/agent/util.rs
+++ crates/corro-agent/src/agent/util.rs
@@ -1046,8 +1046,6 @@ pub async fn process_multiple_changes(
             warn!("process_multiple_changes: commiting transaction took too long - {elapsed:?}");
         }
 
-        // drop connection early
-        drop(conn);
         if let Some(ts) = last_cleared {
             let mut booked_writer = agent
                 .booked()
