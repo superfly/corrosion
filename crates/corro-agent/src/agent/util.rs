44e8f8cdecb0155e57fa30ac6dea011fc5939d17
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2025-05-02 14:57:12 +0100

    use a gauge for processing chunk

--- crates/corro-agent/src/agent/util.rs
+++ crates/corro-agent/src/agent/util.rs
@@ -1143,7 +1143,7 @@ pub async fn process_multiple_changes(
 
     histogram!("corro.agent.changes.processing.time.seconds", "source" => "remote")
         .record(start.elapsed());
-    histogram!("corro.agent.changes.processing.chunk_size").record(change_chunk_size as f64);
+    gauge!("corro.agent.changes.processing.chunk_size").set(change_chunk_size as f64);
 
     Ok(())
 }
