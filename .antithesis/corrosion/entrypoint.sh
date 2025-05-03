7b24ab1d78ed9466fc943b934650d26b23e9488c
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2025-05-03 11:17:34 +0100

    remove sed command in script

--- .antithesis/corrosion/entrypoint.sh
+++ .antithesis/corrosion/entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 # Add gossip and bootstrap addresses to Corrosion config file before 
 # starting agent with Dockerfile CMD
 IP_ADDR=$(ip -4 a s eth0 | grep inet | awk '{print $2}' | cut -d '/' -f 1)
-echo "IP_ADDR: ${IP_ADDR}"
+export IP_ADDR=${IP_ADDR}
 envsubst < /etc/corrosion/config.toml.tpl > /etc/corrosion/config.toml
-sed -i 's/\[gossip\]/&\naddr = "'${IP_ADDR}':8787"/' /etc/corrosion/config.toml
 
+cat /etc/corrosion/config.toml
 su - corrosion
 
 # access admin socket over network
