ca5a65a4f6fb13be40b80d9a6b954522243a249e
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2025-05-03 09:44:41 +0100

    add trigger for test

--- .antithesis/corrosion/entrypoint.sh
+++ .antithesis/corrosion/entrypoint.sh
@@ -4,9 +4,8 @@ set -e
 # starting agent with Dockerfile CMD
 IP_ADDR=$(ip -4 a s eth0 | grep inet | awk '{print $2}' | cut -d '/' -f 1)
 echo "IP_ADDR: ${IP_ADDR}"
-cat /etc/corrosion/config.toml
+envsubst /etc/corrosion/config.toml.tpl > /etc/corrosion/config.toml
 sed -i 's/\[gossip\]/&\naddr = "'${IP_ADDR}':8787"/' /etc/corrosion/config.toml
-cat /etc/corrosion/config.toml
 
 su - corrosion
 
