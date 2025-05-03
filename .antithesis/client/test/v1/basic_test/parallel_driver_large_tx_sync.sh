355c9d3daa2702981c5a4a3da1d423655480c7dd
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2025-05-03 14:10:01 +0100

    fix test script

--- .antithesis/client/test/v1/basic_test/parallel_driver_large_tx_sync.sh
+++ .antithesis/client/test/v1/basic_test/parallel_driver_large_tx_sync.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-CORROSION_API_ADDR=${CORROSION_API_ADDR:-corrosion1:8080}
+addrs=("corrosion1:8080" "corrosion2:8080" "corrosion3:8080")
 
 counts=(10000 1000 900 800 700 600 500 400 300 200 100 1000 900 800 700 600 500
     400 300 200 100 1000 900 800 700 600 500 400 300 200 100 1000 900 800 700
@@ -13,7 +13,9 @@ counts=(10000 1000 900 800 700 600 500 400 300 200 100 1000 900 800 700 600 500
 
 
 for count in ${counts[@]}; do
-    response=$(curl -s -w "\n%{http_code}" -X POST http://${CORROSION_API_ADDR}/v1/transactions -H "Content-Type: application/json" -d "[\"INSERT INTO testsbool (id) WITH RECURSIVE    cte(id) AS (       SELECT random()       UNION ALL       SELECT random()         FROM cte        LIMIT ${count}  ) SELECT id FROM cte;\"]")
+    addr=${addrs[$((RANDOM % 3))]}
+    echo "Count: $count, addr: $addr"
+    response=$(curl -s -w "\n%{http_code}" -X POST http://${addr}/v1/transactions -H "Content-Type: application/json" -d "[\"INSERT INTO testsbool (id) WITH RECURSIVE    cte(id) AS (       SELECT random()       UNION ALL       SELECT random()         FROM cte        LIMIT ${count}  ) SELECT id FROM cte;\"]")
     http_code=$(echo "$response" | tail -n1)
     body=$(echo "$response" | sed '$d')
     # Check if status code is 200
@@ -25,5 +27,3 @@ for count in ${counts[@]}; do
     fi
 done
 
-# wait for all nodes to sync
-sleep 20
--- crates/corrosion/src/admin.rs
+++ crates/corrosion/src/admin.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 
+use eyre::eyre;
 use corro_admin::{Command, LogLevel, Response};
 use futures::{SinkExt, TryStreamExt};
 use tokio::net::UnixStream;
@@ -43,7 +44,7 @@ impl AdminConn {
             match res {
                 None => {
                     error!("Failed to get response from Corrosion's admin!");
-                    break;
+                    return Err(eyre!("Failed to get response from Corrosion's admin!"));
                 }
                 Some(res) => match res {
                     Response::Log { level, msg, ts } => match level {
@@ -55,7 +56,7 @@ impl AdminConn {
                     },
                     Response::Error { msg } => {
                         error!("{msg}");
-                        break;
+                        return Err(eyre!("{msg}"));
                     }
                     Response::Success => {
                         break;
