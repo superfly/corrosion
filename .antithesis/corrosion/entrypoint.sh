614643d4ebbcdea667740809e3e8bdd41ad0f986
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2025-05-03 09:56:11 +0100

    a block step is better

--- .antithesis/corrosion/entrypoint.sh
+++ .antithesis/corrosion/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # starting agent with Dockerfile CMD
 IP_ADDR=$(ip -4 a s eth0 | grep inet | awk '{print $2}' | cut -d '/' -f 1)
 echo "IP_ADDR: ${IP_ADDR}"
-envsubst /etc/corrosion/config.toml.tpl > /etc/corrosion/config.toml
+envsubst < /etc/corrosion/config.toml.tpl > /etc/corrosion/config.toml
 sed -i 's/\[gossip\]/&\naddr = "'${IP_ADDR}':8787"/' /etc/corrosion/config.toml
 
 su - corrosion
