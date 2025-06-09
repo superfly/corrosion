f97ec3a5f035d91a9a372c2465dfe011b1e73bee
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2025-06-09 20:07:51 +0100

    move env flag

--- crates/corro-types/src/change.rs
+++ crates/corro-types/src/change.rs
@@ -5,7 +5,7 @@ pub use corro_api_types::{row_to_change, Change, SqliteValue, TableName};
 use corro_base_types::CrsqlDbVersion;
 use rusqlite::{Connection, OptionalExtension};
 use serde_json::json;
-use tracing::{trace, debug};
+use tracing::{debug, trace};
 
 use crate::{
     agent::{Agent, BookedVersions, ChangeError, VersionsSnapshot},
