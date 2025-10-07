5e48bba33912a83a2e6d4711d64b5d89e7325c50
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2025-10-07 22:51:00 +0100

    check for shutdown on exit in restore script
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- .antithesis/corrosion/test-templates/parallel_driver_restore_corrosion_backup.sh
+++ .antithesis/corrosion/test-templates/parallel_driver_restore_corrosion_backup.sh
@@ -2,22 +2,41 @@
 
 set -e
 
-if [ -f "/var/lib/corrosion/backups/state.db" ]; then
-    exec {lockfd}<>"/var/lib/corrosion/backups/backup.lock"
-    flock "$lockfd"
+check_shutdown() {
+    if [ $? -ne 0 ]; then
+        supervisorctl status corrosion | grep "SHUTDOWN_STATE"
+        if [ $? -eq 0 ]; then
+            echo "Corrosion is shutting down (container might be stopping)"
+            exit 137
+        fi
+    fi
+}
 
-    echo "Restoring backup for corrosion"
-
-    echo "Stopping corrosion first"
-    supervisorctl stop corrosion
-
-    corrosion restore /var/lib/corrosion/backups/state.db
-    echo "Restore done. Starting corrosion"
-    supervisorctl start corrosion
-    supervisorctl restart corro-consul
-
-    rm -f /var/lib/corrosion/backups/state.db
+if [ ! -f "/var/lib/corrosion/backups/state.db" ]; then
+    echo "No backup for corrosion"
     exit 0
 fi
 
-echo "No current backup for corrosion"
+exec {lockfd}<>"/var/lib/corrosion/backups/backup.lock"
+flock "$lockfd"
+
+# a concurrent script might have applied and deleted the state file
+# while we waited for the lock
+if [ ! -f "/var/lib/corrosion/backups/state.db" ]; then
+    echo "Backup already applied and deleted."
+    exit 0
+fi
+
+trap check_shutdown EXIT
+echo "Restoring backup for corrosion"
+
+echo "Stopping corrosion first"
+supervisorctl stop corrosion
+
+corrosion restore /var/lib/corrosion/backups/state.db
+echo "Restore done. Starting corrosion"
+supervisorctl start corrosion
+supervisorctl restart corro-consul
+
+rm -f /var/lib/corrosion/backups/state.db
+exit 0
