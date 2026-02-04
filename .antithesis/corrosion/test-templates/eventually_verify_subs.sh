b88011f1ca23aeb67ffbfc54fa5c3c77b3b09787
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2026-02-04 12:47:44 +0100

    modify dockerfile

--- .antithesis/corrosion/test-templates/eventually_verify_subs.sh
+++ .antithesis/corrosion/test-templates/eventually_verify_subs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls -1 /var/lib/corrosion/subscriptions | xargs -I {} python3 /opt/antithesis/test/v1/basic_test/verify_subscription.py /var/lib/corrosion/state.db {}
+ls -1 /var/lib/corrosion/subscriptions | xargs -I {} python3 /opt/antithesis/py-resources/verify_subscription.py /var/lib/corrosion/state.db {}
