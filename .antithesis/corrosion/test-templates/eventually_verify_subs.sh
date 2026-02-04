9a43bd13fa181d84dce18920ce180319a1c7e7b7
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2026-02-04 12:44:06 +0100

    put in right path

--- .antithesis/corrosion/test-templates/eventually_verify_subs.sh
+++ .antithesis/corrosion/test-templates/eventually_verify_subs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls -1 /var/lib/corrosion/subscriptions | xargs -I {} python3 scripts/verify_subscription.py /var/lib/corrosion/state.db {}
+ls -1 /var/lib/corrosion/subscriptions | xargs -I {} python3 /opt/antithesis/test/v1/basic_test/verify_subscription.py /var/lib/corrosion/state.db {}
