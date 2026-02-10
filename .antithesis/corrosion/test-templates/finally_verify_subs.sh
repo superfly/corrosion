333fd8c08892c120e6580aa5315dd2ef9a25a492
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2026-02-10 22:16:50 +0100

    put retry in finally script
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- .antithesis/corrosion/test-templates/finally_verify_subs.sh
+++ .antithesis/corrosion/test-templates/finally_verify_subs.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-ls -1 /var/lib/corrosion/subscriptions | xargs -I {} python3 /opt/antithesis/py-resources/verify_subscription.py /var/lib/corrosion/state.db {}
+
+for i in {1..30}; do
+    ls -1 /var/lib/corrosion/subscriptions | xargs -I {} python3 /opt/antithesis/py-resources/verify_subscription.py /var/lib/corrosion/state.db {}
+    if [ $? -eq 0 ]; then
+        echo "All subscription databases are consistent"
+        exit 0
+    fi
+
+    echo "Inconsistent data in subscriptions, retrying"
+    sleep 10
+done
+
+echo "Inconsistent data in subscriptions"
+exit 1
