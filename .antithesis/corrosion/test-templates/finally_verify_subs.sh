093abe407259660c96cdcaff481ba6b77fb2f0b6
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2026-02-20 15:20:32 +0100

    add retry to verify_subs
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- .antithesis/corrosion/test-templates/finally_verify_subs.sh
+++ .antithesis/corrosion/test-templates/finally_verify_subs.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-ls -1 /var/lib/corrosion/subscriptions | xargs -I {} python3 /opt/antithesis/py-resources/verify_subscription.py /var/lib/corrosion/state.db {}
+
+set +e
+
+for i in {1..40}; do
+    ls -1 /var/lib/corrosion${i}/subscriptions | xargs -I {} python3 /opt/antithesis/py-resources/verify_subscription.py /var/lib/corrosion${i}/state.db {}
+    if [ $? -eq 0 ]; then
+        echo "Subscription for corrosion${i} is consistent"
+        exit 0
+    fi
+
+    sleep 30
+done
+
+
+exit 1
