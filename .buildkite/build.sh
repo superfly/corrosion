d34b86e9a995a592e23a4e1f5653df9e59d29f5a
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2026-03-09 14:18:01 +0100

    Use bionic sysroot only on buildkite (#441)
    * use bionic sysroot only on buildkite
    
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>
    
    ---------
    
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -2,8 +2,9 @@
 
 set -eux
 
-# build all binaries, optimized
+# build all binaries, optimized with Ubuntu Bionic (18.04) sysroot:
 CFLAGS="--sysroot=/ubuntu-bionic-sysroot" cargo build --verbose --release \
+	--config='target.x86_64-unknown-linux-gnu.rustflags = ["-C", "link-arg=--sysroot=/ubuntu-bionic-sysroot"]' \
 	--bin corrosion
 
 ls -lhA ${CARGO_TARGET_DIR}/release
