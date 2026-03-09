bc3b55895c79bcc2f5613de940753f81e6ed138c
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2026-03-09 13:18:38 +0100

    Handle empty seqs when checking partials in the bookie  (#438)
    * handle empty seqs when checking partials in the bookie
    
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>
    
    * add reconcile gaps
    
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>
    
    * Reconcile gaps for buffered changes too
    
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>
    
    * Get actor_ids from buffered table
    
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>
    
    * fix stray colon
    
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>
    
    ---------
    
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- crates/corro-agent/src/api/peer/mod.rs
+++ crates/corro-agent/src/api/peer/mod.rs
@@ -1351,10 +1351,12 @@ pub async fn parallel_sync(
                                 .increment(changes_len as u64);
 
                             debug!(
-                                "handling versions: {:?}, seqs: {:?}, len: {changes_len} (is_empty: {}) from {actor_id}",
+                               "handling versions: {:?}, actor_id: {:?}, seqs: {:?}, len: {changes_len} (is_empty: {}, is_complete: {}) from {actor_id}",
                                 change.versions(),
+                                change.actor_id,
                                 change.seqs(),
-                                change.is_empty()
+                                change.is_empty(),
+                                change.is_complete()
                             );
                             // only accept emptyset that's from the same node that's syncing
                             if change.is_empty_set() {
